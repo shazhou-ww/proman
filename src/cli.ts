41fb86d047aebbfd246f385b1e21066def75ff0f
小墨 <xiaomooo@shazhou.work>
2026-06-04 02:16:50 +0000

    release: v0.5.1

--- src/cli.ts
+++ src/cli.ts
@@ -8,7 +8,7 @@ import { bump } from './commands/bump.ts'
 import { publish } from './commands/publish.ts'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const VERSION = '0.5.0'
+const VERSION = '0.5.1'
 
 const HELP_TEXT = `Usage: proman <command> [options]
 
